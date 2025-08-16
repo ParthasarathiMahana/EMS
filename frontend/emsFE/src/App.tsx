31846fba5d23cb1d29883452361ba01fb5540f40
Parthasarathi Mahana <parthasarathimahana@gmail.com>
2025-08-16 21:17:53 +0530

    UI for signup page is ready, resposive, theme

--- frontend/emsFE/src/App.tsx
+++ frontend/emsFE/src/App.tsx
@@ -2,11 +2,13 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/auth/login/Login'
 import Signup from './pages/auth/signup/Signup'
+import { ModeToggle } from './components/mode-toggle'
 
 function App() {
 
   return (
     <>
+    <ModeToggle classNames="flex px-2 mt-1 flex-row-reverse"/>
     <BrowserRouter>
       <Routes>
         <Route path='/login' element={<Login />} />
--- frontend/emsFE/src/pages/auth/login/Login.tsx
+++ frontend/emsFE/src/pages/auth/login/Login.tsx
@@ -5,14 +5,15 @@ import { Button } from "../../../components/ui/button";
 import { Label } from "@radix-ui/react-label";
 import { Input } from "../../../components/ui/input";
 import { Link } from "react-router-dom";
-import { ModeToggle } from "../../../components/mode-toggle";
+// import { ModeToggle } from "../../../components/mode-toggle";
 import { useState } from "react";
 import {Eye, EyeOff} from "lucide-react"
 // import Password from "../../../components/ui/password-input";
+import { useTheme } from "../../../components/theme-provider";
 
 const loginSchema = z.object({
-  email: z.email({ message: "Invalid email address" }),
-  password: z.string().min(6, { message: "Password must be at least 6 characters" }),
+  email: z.email({ error: "Invalid email address" }),
+  password: z.string().min(6, { error: "Password must be at least 6 characters" }),
 });
 
 type LoginFormData = z.infer<typeof loginSchema>;
@@ -27,6 +28,9 @@ export default function Login() {
   });
 
   const [showPassword, setShowPassword] = useState(false)
+  const {theme} = useTheme()
+
+  console.log(theme)
 
   const onSubmit = (data: LoginFormData) => {
     console.log("Login Data:", data);
@@ -34,17 +38,18 @@ export default function Login() {
   };
 
   return (
-    <>
-    <ModeToggle classNames="flex px-2 mt-1 flex-row-reverse"/>
+    <div className="flex h-[95vh] items-center">
+    {/* <ModeToggle classNames="flex px-2 mt-1 flex-row-reverse"/> */}
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className="bg-white w-[450px] ml-[calc((100%-450px)/2)] flex flex-col items-center mt-[150px] pb-[64px] px-none rounded-[8px] shadow-[0_0_15px_0px_rgba(0,0,0,0.3)]"
+      className="bg-[var(--card)] w-[90%] sm:w-[400px] md:w-[450px] mx-auto flex flex-col 
+      items-center pb-[64px] px-none rounded-[8px] shadow-[0_0_15px_0px_var(--shadow)]"
     >
             <h1 className="text-3xl font-bold mt-10 mb-2">Login</h1>
       <div className="mb-[8px]">
         <Label htmlFor="email">Email</Label>
         <Input
-        className={`rounded-[4px] h-[32px] w-[350px] mt-[4px] ${errors.email && 'border border-red-500 focus:!ring-red-300'}`}
+        className={`rounded-[4px] h-[32px] w-[350px] mx-auto mt-[4px] ${errors.email && 'border border-red-500 focus:!ring-red-300'}`}
           id="email"
           type="email"
           placeholder="you@example.com"
@@ -58,7 +63,7 @@ export default function Login() {
         {/* password section with show/hide password */}
       <div className="mb-[12px]">
         <Label htmlFor="password">Password</Label>
-        <div className={`shadow-[0_1px_2px_0_rgb(0,0,0,0.05)] w-[350px] h-[32px] flex items-center border  rounded-[4px] ${errors.password && 'border border-red-500 focus:!ring-red-300'}`}>
+        <div className={`shadow-[0_1px_2px_0_rgb(0,0,0,0.05)] mt-[4px] w-[350px] h-[32px] flex items-center border  rounded-[4px] ${errors.password && 'border border-red-500 focus:!ring-red-300'}`}>
         <Input
         className={`border-0 shadow-none rounded-[4px] pr-[40px] h-[32px] w-[300px] ${errors.password && 'focus:!ring-red-300'}`}
 
@@ -84,15 +89,15 @@ export default function Login() {
       </div>
 
       <Button type="submit"
-        className="rounded-[4px] h-[32px] w-[350px] mt-[4px] hover:cursor-pointer border-none"
+        className="rounded-[4px] h-[32px] w-[350px] mt-[8px] hover:cursor-pointer border-none"
       >
         Login
       </Button>
 
       <Link to="/signup">
-        <p className="text-sm mt-8 text-gray-500 hover:text-gray-950 hover:underline">Don't have an account? Sign up</p>
+        <p className="text-sm mt-8 text-[var(--primary)] hover:underline">Don't have an account? Register your company</p>
       </Link>
     </form>
-    </>
+    </div>
   );
 }
