8be8b78270c9db6fe17821e2bfdab2927c83d8e2
Parthasarathi Mahana <parthasarathimahana@gmail.com>
2025-10-13 08:51:50 +0530

    used me and logout api in FE, yet to integrate

--- apps/frontend/src/services/auth.query.ts
+++ apps/frontend/src/services/auth.query.ts
@@ -1,8 +1,8 @@
 import api from '../utils/api'
 import { useQuery, useMutation } from '@tanstack/react-query'
 import { type LoginInput } from '@repo/schemas'
-import { toast } from "sonner";
 
+// ==========================================================(login api)==========================================================
 
 const login = async (payload: LoginInput) => {
     try {
@@ -11,15 +11,14 @@ const login = async (payload: LoginInput) => {
             password: payload.password
         })
 
-        console.log('succssfully logged in', response);    
+        // console.log('succssfully logged in', response);    
         return response
     } catch (error: any) {
-        console.log(error);
+        // console.log(error);
         return error?.response
     }
 
 }
-
 
 export const useLogin = () => {
     const mutate = useMutation(
@@ -28,7 +27,46 @@ export const useLogin = () => {
             mutationKey: ['userlogin']
         }
     )
-
     return mutate
 }
 
+// ==========================================================(me api)==========================================================
+
+const showStatus = async () => {
+    try {
+        const response = api.get('/auth/me')
+        return response
+    } catch (error) {
+        return error
+    }
+}
+
+export const useShowStatus = () => {
+    const query = useQuery({
+        queryKey: ['showStatus'],
+        queryFn: showStatus
+    })
+
+    return query
+}
+
+
+// ==========================================================(logout api)==========================================================
+
+const logout = async () => {
+    try {
+        const response = api.get('/logout')
+        return response
+    } catch (error) {
+        return error
+    }
+}
+
+export const useLogout = () => {
+    const query = useQuery({
+        queryKey: ['logout'],
+        queryFn: logout
+    })
+
+    return query
+}
